f3ddcf0cbf99d15458f508892c51624cf3cbac73
mknudson3 <mknudson3@gatech.edu>
2026-07-02 12:35:38 -0400

    add affordance (UI only) for adding NFC tag to habit

--- app/src/main/java/mk/habittracker/ui/AddHabitScreen.kt
+++ app/src/main/java/mk/habittracker/ui/AddHabitScreen.kt
@@ -3,24 +3,43 @@ package mk.habittracker.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddHabitScreen(
     onDismiss: () -> Unit
 ) {
+    val nfcBottomSheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     FullSheet(
         title = "Add a habit",
         onBack = onDismiss,
@@ -28,6 +47,22 @@ fun AddHabitScreen(
         Column {
             Text("Add habit", style = MaterialTheme.typography.displayMedium)
         }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { showBottomSheet = true}
+        ) {
+            Icon(
+                imageVector = Icons.Default.Build,
+                contentDescription = "",
+            )
+            Text("Link NFC tag (optional)")
+        }
+    }
+    if (showBottomSheet) {
+        NfcBottomSheet(
+            state = nfcBottomSheetState,
+            onDismiss = { showBottomSheet = false },
+        )
     }
 }
 
@@ -56,6 +91,36 @@ fun FullSheet(
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
             content()
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NfcBottomSheet(
+    state: SheetState,
+    onDismiss: () -> Unit
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        sheetState = state,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Default.Info, contentDescription = ""
+            )
+            Text("Ready to scan", style = MaterialTheme.typography.displaySmall)
+            Text("Hold your phone near the NFC tag")
+            TextButton(
+                onClick = onDismiss,
+
+                ) {
+                Text("Cancel")
+            }
         }
     }
 }
--- app/src/main/java/mk/habittracker/ui/HabitDetailScreen.kt
+++ app/src/main/java/mk/habittracker/ui/HabitDetailScreen.kt
@@ -1,5 +1,6 @@
 package mk.habittracker.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,12 +8,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,12 +47,17 @@ fun HabitDetailScreen(
     } ?: Text("error fetching habit with id ${vm.habitId}")
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HabitDetailScreen(
     onBack: () -> Unit,
     title: String,
     nCheckIns: String,
 ) {
+    val nfcBottomSheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     FullSheet(
         title = title,
         onBack = onBack,
@@ -51,15 +66,43 @@ fun HabitDetailScreen(
             modifier = Modifier.fillMaxSize().padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            NfcSection()
+            NfcSection(
+                onClick = { showBottomSheet = true }
+            )
             HistorySection(nCheckIns = nCheckIns)
+        }
+    }
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = nfcBottomSheetState,
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Info, contentDescription = ""
+                )
+                Text("Ready to scan", style = MaterialTheme.typography.displaySmall)
+                Text("Hold your phone near the NFC tag")
+                TextButton(
+                    onClick = { showBottomSheet = false },
+
+                ) {
+                    Text("Cancel")
+                }
+            }
         }
     }
 }
 
 @Composable
-fun NfcSection() {
-    Card(modifier = Modifier.fillMaxWidth()) {
+fun NfcSection(onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .clickable(onClick = onClick)
+        .fillMaxWidth()
+    ) {
         Text("PHYSICAL TRIGGER", style = MaterialTheme.typography.titleMedium)
         Row {
             Icon(
