3dd96fcb62d0cd685e1d806f356b2df0f5567ba1
mknudson3 <mknudson3@gatech.edu>
2026-08-20 21:27:15 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mk.habittracker"
         minSdk = 28
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "com.mk.habittracker.HiltTestRunner"
     }
